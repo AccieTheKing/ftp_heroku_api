4cc545400a47fbb8d499558d8e564475dacf12f4
Acdaling <acdaling@gmail.com>
2020-02-23 02:59:39 +0100

    changed allow origin

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const routes = require('./routes/index');
 app.use(require('express').json());
 
 app.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Origin', 'https://projects.acdaling.nl/ftp_client/');
   res.header('Access-Control-Allow-Methods', '*');
   res.header('Access-Control-Allow-Headers', '*');
   next();
